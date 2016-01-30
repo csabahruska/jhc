c31e32eb77fbec8b0ed7f79a893207d6feb7c51b
Csaba Hruska <csaba.hruska@gmail.com>
2016-01-30 23:09:18 +0100

    binary instance for Grin

--- jhc-0.8.2/src/Util/Perhaps.hs
+++ jhc-0.8.2/src/Util/Perhaps.hs
@@ -3,9 +3,13 @@ module Util.Perhaps where
 import Data.Typeable
 import Data.Monoid
 
-data Perhaps = No | Maybe | Yes
-    deriving(Show,Read,Typeable,Eq,Ord)
+import Data.Binary
+import GHC.Generics (Generic)
 
+data Perhaps = No | Maybe | Yes
+    deriving(Show,Read,Typeable,Eq,Ord,Generic)
+
+instance Binary Perhaps
 
 -- the greatest lower bound was chosen as the Monoid
 -- the least upper bound is just the maximum under Ord
